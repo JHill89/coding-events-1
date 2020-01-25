528909b641f2bb7f8e0249d0f5a92345483fa5b5
Kristopher Johnson <krazyk786@gmail.com>
2020-01-25 16:22:16 -0600

    added equals method to EventCategory class

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -30,4 +30,19 @@ public class EventCategory {
     public void setName(String name) {
         this.name = name;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        EventCategory that = (EventCategory) o;
+    
+        return id == that.id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
